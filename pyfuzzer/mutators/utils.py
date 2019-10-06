da2b8d55181605d7c5743b495f1487ec0ce41675
Erik Moqvist <erik.moqvist@gmail.com>
2019-10-06 11:32:13 +0200

    Limit print corpus output length.

--- pyfuzzer/mutators/utils.py
+++ pyfuzzer/mutators/utils.py
@@ -1,7 +1,7 @@
 import traceback
 
 
-def print_function(func, args):
+def print_function(func, args, limit=1024):
     sargs = []
 
     for arg in args:
@@ -10,13 +10,13 @@ def print_function(func, args):
         else:
             arg = str(arg)
 
-        sargs.append(arg)
+        sargs.append(arg[:limit])
 
     fargs = ', '.join(sargs)
 
     try:
         res = func(*args)
-        print(f'{func.__name__}({fargs}) = {res}')
+        print(f'{func.__name__}({fargs}) = {str(res)[:limit]}')
     except Exception:
         print(f'{func.__name__}({fargs}) raises:')
         traceback.print_exc()
